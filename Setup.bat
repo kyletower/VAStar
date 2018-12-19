9507ad09d7ad6c7993060fca3a1c5f8dfb6a623c
kyletower <ktower@hcps.us>
2018-12-19 11:46:06 -0500

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -78,6 +78,7 @@ GOTO End_Routine
 
 :Install_Chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+PAUSE
 choco install -y adobereader
 choco install -y flashplayerplugin
 choco install -y googlechrome-allusers
